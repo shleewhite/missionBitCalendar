6d85c8f9302ebed16045ffa011f423f7a9338ccb
Lee White <lwhite@sportsbasement.com>
2015-02-12 01:43:19 +0000

    added names/dates to feb, just dates to mar, updated css. need to decide format of individual dates and get the border to work in the individual month tables

---
+++ _site/views/behavior.js
@@ -0,0 +1,12 @@
+
+(function () {
+
+    function onMonthClick(event) {
+        var target = $(event.target);
+        var x = target.data('monthName');
+        document.location.href= x + '.html';
+    }
+    
+    $(document).on('click', '.month', onMonthClick);
+    
+}());
